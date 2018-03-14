d1925b7bba643939e914af49a742d4788a1df6c0
unknown <mohsen.samadani@gmail.com>
2018-03-14 14:45:17 -0400

    smooth path

--- src/main.cpp
+++ src/main.cpp
@@ -357,7 +357,7 @@ int main() {
 
             // Fill up the rest of our path planner after filling it with previous points, here we will always output 50 points.
             for (int i = 1; i <= 50 - previous_path_x.size(); i++) {
-              double N = (target_dist/(0.2 * ref_vel / 2.24));
+              double N = (target_dist/(0.02 * ref_vel / 2.24));
               double x_point = x_add_on + target_x/N;
               double y_point = s(x_point);
 
